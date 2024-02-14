8b50dd6a34e392174effb73576f2a4a1751f4781
alenapech <alenaryzhova@inbox.ru>
2024-02-15 04:22:42 +0600

    lesson1 hw

--- src/test/java/seminar/GeekBrainsTests.java
+++ src/test/java/seminar/GeekBrainsTests.java
@@ -1,13 +1,14 @@
 package seminar;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +19,8 @@ public class GeekBrainsTests {
     private WebDriverWait wait;
     private static String USERNAME;
     private static String PASSWORD;
+
+    private static final String SCREENSHOT_PATH = "src\\test\\resources";
 
     @BeforeAll
     static void beforeAll() {
@@ -33,8 +36,7 @@ public class GeekBrainsTests {
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @Test
-    void loginTest() {
+    private void login() {
         driver.get("https://test-stand.gb.ru/login");
         wait.until(ExpectedConditions.presenceOfElementLocated(
                 By.cssSelector("form#login input[type='text']"))).sendKeys(USERNAME);
@@ -48,6 +50,51 @@ public class GeekBrainsTests {
         WebElement usernameLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText(USERNAME)));
         assertEquals(String.format("Hello, %s", USERNAME)
                 , usernameLink.getText().replace("\n", " ").trim());
+    }
+
+    @Test
+    void loginTest() {
+        login();
+    }
+
+    @Test
+    void dummyCreationTest() {
+        login();
+        WebElement addButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='create-btn']")));
+        addButton.click();
+
+        String login = String.valueOf(System.currentTimeMillis());
+        String firstName = String.format("First Name %s", login);
+        String lastName = String.format("Last Name %s", login);
+
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//*[@id='upsert-item']//span[contains(text(), 'Fist Name')]/../input[@type='text']"))) //here we have misprint on the form
+                .sendKeys(firstName);
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//*[@id='upsert-item']//span[contains(text(), 'Last Name')]/../input[@type='text']")))
+                .sendKeys(lastName);
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//*[@id='upsert-item']//span[contains(text(), 'Login')]/../input[@type='text']")))
+                .sendKeys(login);
+
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id='upsert-item']//span[contains(text(), 'Save')]//..//..//button"));
+        loginButton.click();
+
+        WebElement createdName = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(String.format("//table[@aria-label='Dummies list']/tbody/tr/td[contains(text(), '%s')]", login))));
+        assertEquals(String.format("%s %s", lastName, firstName), createdName.getText().replace("\n", " ").trim());
+
+        saveScreenshot(login);
+    }
+
+    private void saveScreenshot(String name) {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File(SCREENSHOT_PATH + name + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
